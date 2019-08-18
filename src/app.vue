0468ab29728f5f9c392701eb039761706823f050
Sandy66666 <dongshuang_1992@163.com>
2019-08-18 21:12:31 +0800

    新增文件

---
+++ src/app.vue
@@ -0,0 +1,20 @@
+<template>
+    <div id="textClass">{{text}}</div>
+</template>
+
+<script>
+    export default {
+        name: "app",
+        data () {
+            return {
+                text: "我是一个text！"
+            }
+        }
+    }
+</script>
+
+<style scoped>
+#textClass {
+    color: brown;
+}
+</style>
