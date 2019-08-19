595607e0f19ad245575ae770751e82fa7cfcaea1
Sandy66666 <dongshuang_1992@163.com>
2019-08-19 22:14:51 +0800

    提交第一版

--- src/index.js
+++ src/index.js
@@ -1,1 +1,6 @@
 import Vue from 'vue';
+
+new Vue({
+    el: '#app',
+    render: h=>h(App)
+})
--- webpack.config.js
+++ webpack.config.js
@@ -3,3 +3,19 @@
  * Date: 2019/8/17
  * Description:
  */
+const path = require('path');
+module.exports = {
+    entry: path.join(__dirname,'src/index.js'),
+    output: {
+        filename: 'bundle.js',
+        path: path.join(__dirname,'dist')
+    },
+    module: {
+        rules: [
+            {
+                test:/.vue$/,
+                loader: 'vue-loader'
+            }
+        ]
+    }
+}
